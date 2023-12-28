5f988c5d89b361e98128b23e6e814ccafd0ef589
oussenimbohou <oussenimbohou@gmail.com>
2023-12-28 18:40:31 +0100

    successfully handle API-Gateway configuration and all the endpoints can be accessed via the gateway

--- product-service/src/main/java/com/barack/springbootmicroservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.barack.springbootmicroservice.repository;
-
-import com.barack.springbootmicroservice.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
